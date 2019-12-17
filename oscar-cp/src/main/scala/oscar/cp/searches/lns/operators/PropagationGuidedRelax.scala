e6c7769b4fbec2b9d4a7666be301e73d19b4f66f
Charles Thomas <cftmthomas@gmail.com>
2019-12-17 19:05:03 +0100

    Fixing PropagationGuided relaxation

--- oscar-cp/src/main/scala/oscar/cp/searches/lns/operators/PropagationGuidedRelax.scala
+++ oscar-cp/src/main/scala/oscar/cp/searches/lns/operators/PropagationGuidedRelax.scala
@@ -1,163 +1,99 @@
 package oscar.cp.searches.lns.operators
 
 import oscar.algo.Inconsistency
+import oscar.algo.reversible.ReversibleInt
 import oscar.cp.{CPIntVar, CPSolver}
 import oscar.cp.searches.lns.CPIntSol
 
 import scala.collection.mutable
 import scala.util.Random
 
-object PropagationGuidedRelax{
-  var N = 0
-  lazy val closeness: PropagationGuidedRelax = new PropagationGuidedRelax(N) // Closeness store used for propagation guided relax
-
-  /**
-    * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
-    * @param s The estimated size of the neighbourhood to attain.
-    */
-  def propagationGuidedRelax(solver: CPSolver, vars: Iterable[CPIntVar], currentSol: CPIntSol, s: Double): Unit = {
-    if(N == 0) N = vars.size
-    //    println("relaxing to size " + s)
-    val varSeq = vars.toSeq
-    val varArray = varSeq.indices.toArray //map to real index of variables
-    var boundStart = varArray.length //Elements of varArray from this index are bound
-    val domainSize = varSeq.map(v => v.size).toArray //initial domain size of each variable
-    val prevDomainSize = domainSize.clone()
-    var size = domainSize.map(i => math.log(i)).sum //Current estimation of the search space obtained
-    var toFreezeNext = -1 //next var to freeze (most impacted by previous propagation)
-
-    while (size > s) {
-
-      val next = if (toFreezeNext == -1) varArray(Random.nextInt(boundStart)) //If no var to freeze next, selecting random var
-      else toFreezeNext
-      solver.add(varSeq(next) === currentSol.values(next)) //Freezing var
-      // propagation should be called as var is frozen
-      if (!varSeq(next).isBound) throw Inconsistency
-
-      //Updating bounded vars and var to freeze:
-      toFreezeNext = -1
-      var maxImpact = 0
-      val propagation = Array.fill[Double](varSeq.length){-1.0} //Propagation impact (vars already bound are ignored)
-      var i = 0
-      size = 0.0
-
-      while (i < boundStart) {
-        val x = varArray(i)
-        val domSizeX = varSeq(x).size
-        propagation(x) = (prevDomainSize(x) - domSizeX).toDouble / domainSize(x)
-        prevDomainSize(x) = domSizeX
-        size += math.log(varSeq(x).size)
-
-        if (varSeq(x).isBound) {
-          boundStart -= 1
-          varArray(i) = varArray(boundStart)
-          varArray(boundStart) = x //marking var as bound
-        }
-
-        else if (domSizeX < domainSize(x)) {
-          val impact = domainSize(x) - domSizeX
-          if (impact > maxImpact) {
-            toFreezeNext = x
-            maxImpact = impact
-          } //If the var is the most impacted, selecting it as next to freeze
-          i += 1
-        }
-
-        else i += 1
-      }
-
-      closeness.update(next, propagation)
-    }
-
-    //    println("relaxation done, " + (varSeq.length - boundStart) + " vars frozen")
-  }
-
-  /**
-    * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
-    * @param s The estimated size of the neighbourhood to attain.
-    */
-  def reversedPropagationGuidedRelax(solver: CPSolver, vars: Iterable[CPIntVar], currentSol: CPIntSol, s: Double): Unit = {
-    if(N == 0) N = vars.size
-    //    println("relaxing to size " + s)
-    val varSeq = vars.toSeq
-    val varArray = varSeq.indices.toArray //map to real index of variables
-
-    var next = Random.nextInt(varSeq.length) //Selecting randomly first var to relax
-    varArray(next) = varSeq.length-1
-    varArray(varSeq.length-1) = next
-
-    var relaxStart = varArray.length-1 //Elements of varArray from this index are part of the relaxed variables
-    var avgSize = varSeq(next).size
-    var size = math.log(avgSize) //Current estimation of the search space obtained
-    var subset = closeness.getCloseSubset(next, (math.round(s - size) / avgSize).toInt) //Subset of next
-
-    while (size < s && relaxStart > 0) {
-
-      if(subset.isEmpty){ //No more element in subset:
-        next = Random.nextInt(relaxStart) //Selecting new random var as next
-        subset = closeness.getCloseSubset(next, 10) //Retrieving subset of this var
-      }
-      else{
-        next = subset.head
-        subset -= next
-      }
-
-      relaxStart -= 1
-      varArray(next) = varArray(relaxStart)
-      varArray(relaxStart) = next
-      val relaxed = varSeq(next).size
-      avgSize = (avgSize * (varSeq.length - relaxStart - 1) + relaxed) / (varSeq.length - relaxStart)
-      size += math.log(relaxed)
-    }
-
-    for(i <- (0 until relaxStart).map(x => varArray(x))){
-      solver.add(varSeq(i) === currentSol.values(i)) //Freezing var
-      if (!varSeq(i).isBound) throw Inconsistency
-    }
-
-    //    println("relaxation done, " + relaxStart + " vars frozen")
-  }
-}
-
 /**
   * This class keeps track of the closeness relationship between variables. The closeness is defined as the average
   * volume of propagation that is involved on one variable when another is instantiated.
   */
-//TODO: Manage memory better
-class PropagationGuidedRelax(val size: Int){
-  //If too much variables, these arrays are way to big!
-  val nImpacted: Array[Array[Int]] = Array.tabulate(size, size){ (_, _) => 0}
-  val closeness: Array[Array[Double]] = Array.tabulate(size, size){ (i, j) => if(i == j) 1.0 else 0.0}
+class PropagationGuidedRelax(variables: Seq[CPIntVar]){
+  val nVars: Int = variables.length
+
+  //Data structure to keep track of domain size:
+  val startDomainSize: Array[Int] = Array.tabulate(nVars)(i => variables(i).size)
+  val prevDomainSize: Array[ReversibleInt] = Array.tabulate(nVars)(i => ReversibleInt(variables(i).size)(variables(i).store))
+
+  //Data structure to keep track of closeness:
+  //TODO: If too much variables, these arrays are way to big! => Use better structure
+  val nImpacted: Array[Array[Int]] = Array.fill(nVars, nVars)(0)
+  val closeness: Array[Array[Double]] = Array.tabulate(nVars, nVars){ (i, j) => if(i == j) 1.0 else 0.0}
 
   /**
     * Updates the closeness store with the propagation given an instantiated variable.
     * @param instantiated the var that has been instantiated.
-    * @param propagation the propagation for each other var (a negative value means that the var was already
-    *                    instantiated and thus it should not be updated).
     */
-  def update(instantiated: Int, propagation: Array[Double]): Unit ={
-    for(i <- propagation.indices) if(i != instantiated && propagation(i) >= 0.0){
+  //TODO: automatically call this when assigning var in search (use listener?)
+  def updateCloseness(instantiated: Int): Unit ={
+    for(i <- variables.indices) if(i != instantiated){
+      val currentDomSize = variables(i).size
 
-      //Updating average propagation:
-      var newCloseness = (closeness(instantiated)(i) * nImpacted(instantiated)(i) + propagation(i)) / (nImpacted(instantiated)(i) + 1)
-      closeness(instantiated)(i) = newCloseness
-      nImpacted(instantiated)(i) +=1
+      //If domain size has changed:
+      if(currentDomSize < prevDomainSize(i).value){
+        val impact = (prevDomainSize(i) - currentDomSize).toDouble / startDomainSize(i)
+
+        //Updating closeness:
+        closeness(instantiated)(i) = (closeness(instantiated)(i) * nImpacted(instantiated)(i) + impact) / (nImpacted(instantiated)(i) + 1)
+        nImpacted(instantiated)(i) +=1
+
+        prevDomainSize(i).setValue(currentDomSize)
+      }
     }
+//    println("closeness:\n" + closeness.map(_.mkString(", ")).mkString("\n"))
+  }
+
+  /**
+   * Updates the closeness store with the propagation given an instantiated variable.
+   * @param instantiated the var that has been instantiated.
+   * @return the index of the most impacted unbound variable (-1 if no variable has been impacted)
+   */
+  def updateClosenessAndGetMostImpacted(instantiated: Int): Int ={
+    var maxImpact = 0.0
+    var mostImpacted = -1
+
+    for(i <- variables.indices) if(i != instantiated){
+      val currentDomSize = variables(i).size
+
+      //If domain size has changed:
+      if(currentDomSize < prevDomainSize(i).value){
+        val impact = (prevDomainSize(i) - currentDomSize).toDouble / startDomainSize(i)
+
+        //Updating closeness:
+        closeness(instantiated)(i) = (closeness(instantiated)(i) * nImpacted(instantiated)(i) + impact) / (nImpacted(instantiated)(i) + 1)
+        nImpacted(instantiated)(i) +=1
+
+        //Checking impact:
+        if(!variables(i).isBound && impact > maxImpact){
+          mostImpacted = i
+          maxImpact = impact
+        }
+
+        prevDomainSize(i).setValue(currentDomSize)
+      }
+    }
+//    println("closeness:\n" + closeness.map(_.mkString(", ")).mkString("\n"))
+
+    mostImpacted
   }
 
   /**
     * Get the closeness between vars i and j.
-    * @return the closeness between i and j or 0.0 if i or j are not valid indexes.
+    * @return the closeness between i and j or 0.0 if i or j are not valid indices.
     */
-  def getCloseness(i:Int, j:Int): Double = if(i < size && j < size) closeness(i)(j) else 0.0
+  def getCloseness(i:Int, j:Int): Double = if(i < nVars && j < nVars) closeness(i)(j) else 0.0
 
   /**
-    * Get the close vars of i.
+    * Get the close vars (closeness > 0) of i.
     * @return a list of the close vars of i (empty if no var is close or i is not a valid index).
     */
   def getClose(i: Int): Iterable[Int] ={
     var closeSet = new mutable.ListBuffer[Int]
-    if(!(i < size)) return closeSet
+    if(i >= nVars) return closeSet
 
     for(v <- closeness(i).indices) if(v != i && closeness(i)(v) > 0.0)
       closeSet += v
@@ -171,9 +107,9 @@ class PropagationGuidedRelax(val size: Int){
     */
   def getCloseSubset(initialVar: Int, maxSubsetSize: Int): mutable.LinkedHashSet[Int]= {
     val subset = new mutable.LinkedHashSet[Int]  //Current subset
-    if(initialVar >= size) return subset
+    if(initialVar >= nVars) return subset
 
-    val closeToSubset = Array.fill[Double](size){0.0}
+    val closeToSubset = Array.fill[Double](nVars){0.0}
     val toAddNext = new mutable.PriorityQueue[Int]()(Ordering.by[Int, Double](i => closeToSubset(i)))
     for(v <- getClose(initialVar)){
       closeToSubset(v) = closeness(initialVar)(v)
@@ -194,12 +130,121 @@ class PropagationGuidedRelax(val size: Int){
     subset
   }
 
+  /**
+   * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
+   * @param s The estimated size of the neighbourhood to attain.
+   */
+  def propagationGuidedRelax(solver: CPSolver, currentSol: CPIntSol, s: Double): Unit = {
+    //    println("relaxing to size " + s)
+    val varArray = Array.tabulate(variables.length)(i => i) //map to real index of variables
+    var boundStart = varArray.length //Elements of varArray from this index are bound
+    var size = variables.map(v => math.log(v.size)).sum //Current estimation of the search space obtained
+    var toFreezeNext = -1 //next var to freeze (most impacted by previous propagation)
+
+    while (size > s) {
+
+      val next = if (toFreezeNext == -1) varArray(Random.nextInt(boundStart)) //If no var to freeze next, selecting random var
+      else toFreezeNext
+      solver.add(variables(next) === currentSol.values(next)) //Freezing var => propagation occurs
+      // propagation should be called as var is frozen
+      if (!variables(next).isBound) throw Inconsistency
+
+      //Updating closeness and next var to freeze:
+      toFreezeNext = updateClosenessAndGetMostImpacted(next)
+
+      //Updating size and bounded vars:
+      size = 0.0
+      var i = 0
+      while (i < boundStart) {
+        val x = varArray(i)
+        size += math.log(variables(x).size) //Updating size
+
+        //Marking var as bound:
+        if (variables(x).isBound) {
+          boundStart -= 1
+          varArray(i) = varArray(boundStart)
+          varArray(boundStart) = x
+        }
+        else i += 1
+      }
+    }
+//    println("relaxation done, " + (variables.length - boundStart) + " vars frozen")
+  }
+
+  /**
+   * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
+   * @param s The estimated size of the neighbourhood to attain.
+   */
+  def reversedPropagationGuidedRelax(solver: CPSolver, currentSol: CPIntSol, s: Double): Unit = {
+    //    println("relaxing to size " + s)
+    val varArray = Array.tabulate(variables.length)(i => i) //map to real index of variables
+
+    var next = Random.nextInt(variables.length) //Selecting randomly first var to relax
+    varArray(next) = variables.length-1
+    varArray(variables.length-1) = next
+
+    var relaxStart = varArray.length-1 //Elements of varArray from this index are part of the relaxed variables
+    var avgSize = variables(next).size
+    var size = math.log(avgSize) //Current estimation of the search space obtained
+    var subset = getCloseSubset(next, (math.round(s - size) / avgSize).toInt) //Subset of next
+
+    while (size < s && relaxStart > 0) {
+
+      if(subset.isEmpty){ //No more element in subset:
+        next = Random.nextInt(relaxStart) //Selecting new random var as next
+        subset = getCloseSubset(next, 10) //Retrieving subset of this var
+      }
+      else{
+        next = subset.head
+        subset -= next
+      }
+
+      relaxStart -= 1
+      varArray(next) = varArray(relaxStart)
+      varArray(relaxStart) = next
+      val relaxed = variables(next).size
+      avgSize = (avgSize * (variables.length - relaxStart - 1) + relaxed) / (variables.length - relaxStart)
+      size += math.log(relaxed)
+    }
+
+    for(i <- (0 until relaxStart).map(x => varArray(x))){
+      solver.add(variables(i) === currentSol.values(i)) //Freezing var => propagation occurs
+      if (!variables(i).isBound) throw Inconsistency
+      updateCloseness(i)
+    }
+//    println("relaxation done, " + relaxStart + " vars frozen")
+  }
+
   override def toString: String = {
     var str = ""
-    for(i <- 0 until size){
-      for(j <- 0 until size) str += "(" + closeness(i)(j) + ", " + nImpacted(i)(j) + ")  "
+    for(i <- 0 until nVars){
+      for(j <- 0 until nVars) str += "(" + closeness(i)(j) + ", " + nImpacted(i)(j) + ")  "
       str += "\n"
     }
     str
   }
+}
+
+object PropagationGuidedRelax{
+  var varSeq: Seq[CPIntVar] = Seq()
+  lazy val propGuidedStore: PropagationGuidedRelax = new PropagationGuidedRelax(varSeq) // Closeness store used for propagation guided relax
+
+  /**
+   * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
+   * @param s The estimated size of the neighbourhood to attain.
+   */
+  def propagationGuidedRelax(solver: CPSolver, vars: Iterable[CPIntVar], currentSol: CPIntSol, s: Double): Unit = {
+    if(varSeq.isEmpty) varSeq = vars.toSeq
+    propGuidedStore.propagationGuidedRelax(solver, currentSol, s)
+  }
+
+  /**
+   * Relaxes variables using propagation to guide the relaxation until the estimated size s of the neighbourhood is attained.
+   * @param s The estimated size of the neighbourhood to attain.
+   */
+  def reversedPropagationGuidedRelax(solver: CPSolver, vars: Iterable[CPIntVar], currentSol: CPIntSol, s: Double): Unit = {
+    if(varSeq.isEmpty) varSeq = vars.toSeq
+    propGuidedStore.reversedPropagationGuidedRelax(solver, currentSol, s)
+  }
+
 }
